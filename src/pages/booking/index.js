53fb0306253d0667ac14511c0dcc4bfd6ecd849f
ParimalTank <parimal183@webmail.rainit.in>
2023-10-12 12:40:46 +0530

    Date

--- src/pages/booking/index.js
+++ src/pages/booking/index.js
@@ -2,22 +2,33 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import React from 'react';
+import { Box } from '@mui/material';
+import moment from 'moment';
 
 export default function Booking() {
 
     const [value, setValue] = React.useState(null);
+    var epoch = moment(value, "DD/MM/YYYY").unix();
+    console.log("value: ", value);
+    console.log("This is epoch", epoch);
+
+    const date = moment(value).format("DD/MM/YYYY");
+    console.log("date: ", date);
 
     return (
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker
-                label="Basic example"
-                value={value}
-                onChange={(newValue) => {
-                    setValue(newValue);
-                }}
-                renderInput={(params) => <TextField {...params} />
-                }
-            />
-        </LocalizationProvider>
+        <Box sx={{ display: "flex", justifyContent: "center", mt: "50px" }}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                    label="Select Slot"
+                    format="DD/MM/YYYY"
+                    value={value}
+                    onChange={(newValue) => {
+                        setValue(newValue);
+                    }}
+                    renderInput={(params) => <TextField {...params} />
+                    }
+                />
+            </LocalizationProvider>
+        </Box>
     );
 }
